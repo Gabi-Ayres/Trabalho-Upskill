35f1877e9200662b91e078186f46d08289a02cb7
Gabriella Maia Pimentel Ayres <gabi.maia.pimentel@gmail.com>
2025-11-06 12:32:03 +0000

    feat: Mudado numero da soma

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   console.log("Olá! Bem-vindos à programação!");
 
   // Passo 2: Mostrar a soma (Processamento Simples)
-  console.log(10 + 5); 
+  console.log(10 + 9); 
 
   // Passo 3: Mostrar um valor Booleano (revisão da Aula 1)
   console.log(true);
